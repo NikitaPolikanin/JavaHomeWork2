66a738bdcca42bf00959529f1fae41d804b1d3fb
Nikita_Polikanin <n.s.polikanin@gmail.com>
2023-04-14 23:05:20 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 25897;
+        int bonusStep = 20;
+        int bonus = amount / bonusStep;
+
+        System.out.println("Bonus Miles: " + bonus);
+    }
+}
